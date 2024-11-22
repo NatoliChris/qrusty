a434207021000e66148527e07173f3b73add6281
Chris Natoli <natolichris@gmail.com>
2024-11-22 16:00:50 +1100

    comments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
-use std::fs::Metadata;
+//! # QRusty
+//!
+//! QRusty the qrab, a small applet to read QRs on the screen and present them
+//! either as text or copied directly to the clipboard.
+//!
+//! Right now it's not battle tested, nor is it ready for production.
+//!
+//! ## Commands
+//!
+//! * `-s` : select with the mouse by click and dragging a bounding box.
+//! * `-c` : copy the identified QR code into clipboard (note, multiple QRs
+//!          will lead to a space-delimited array copied)
 
 use bounding_box::BoundingBox;
 use clap::{Arg, ArgAction, Command};
@@ -17,7 +28,14 @@ struct MouseBox {
     end_y: i32,
 }
 
-// Get the user to select a region on the screen
+/// Get a selection from a user
+///
+/// The user clicks and drags their mouse to create a bounding
+/// rectangle.
+/// TODO: find a way to provide a visual of the rectangle as the user
+/// is drawing it.
+/// TODO: find a way to act like `scrot -s`; have the tool capture the click
+/// and drag so that it doesn't interact with the page, etc.
 fn get_region_from_user() -> Result<MouseBox, ()> {
     // variables for tracking
     let mut init_selection = false;
@@ -65,10 +83,12 @@ fn get_region_from_user() -> Result<MouseBox, ()> {
     Ok(sc_box)
 }
 
-// Find what monitor the selection was in.
-fn find_monitor_box(selection_bound: &BoundingBox) -> Result<Monitor, ()> {
-    // TODO: find a way to morph multiple monitors for screenshots over multiple screens!
-
+/// Get which monitor the selection falls on.
+///
+/// This attempts to find which monitor the mouse's selection falls on.
+/// TODO: find a way to morph multiple monitors for screenshots over multiple
+/// screens
+fn find_monitor_selected(selection_bound: &BoundingBox) -> Result<Monitor, ()> {
     if let Ok(m_list) = Monitor::all() {
         for mon in m_list {
             // Create the monitor bounding box
@@ -80,12 +100,14 @@ fn find_monitor_box(selection_bound: &BoundingBox) -> Result<Monitor, ()> {
                 return Ok(mon);
             }
         }
-        Err(())
-    } else {
-        Err(())
     }
+    Err(())
 }
 
+/// List all the monitors
+///
+/// List all onitors returning a list of the monitors - used in the tool
+/// when default is to check QRs on all screens.
 fn list_all_monitors() -> Result<Vec<Monitor>, ()> {
     if let Ok(m_list) = Monitor::all() {
         return Ok(m_list);
@@ -95,6 +117,11 @@ fn list_all_monitors() -> Result<Vec<Monitor>, ()> {
     Err(())
 }
 
+/// Grab the screen at the selected area from the mouse
+///
+/// This will crop the screen area to the mouse selection.
+/// NOTE: there are a few potential bugs here with crossing monitors, still
+/// have to fix them up.
 fn get_screen_shot(selection: &MouseBox) -> Result<RgbaImage, ()> {
     let selection_bound = BoundingBox::new_from_coords(
         selection.start_x,
@@ -103,7 +130,7 @@ fn get_screen_shot(selection: &MouseBox) -> Result<RgbaImage, ()> {
         selection.end_y,
     );
 
-    let maybe_monitor = find_monitor_box(&selection_bound);
+    let maybe_monitor = find_monitor_selected(&selection_bound);
     if let Ok(monitor) = maybe_monitor {
         let maybe_ss = monitor.capture_image();
 
@@ -132,6 +159,9 @@ fn get_screen_shot(selection: &MouseBox) -> Result<RgbaImage, ()> {
     Err(())
 }
 
+/// Screen grab all the monitors available
+///
+/// Will take a screenshot of all monitors
 fn screenshot_all_monitors() -> Result<Vec<RgbaImage>, ()> {
     let maybe_monitors = list_all_monitors();
 
@@ -146,6 +176,14 @@ fn screenshot_all_monitors() -> Result<Vec<RgbaImage>, ()> {
     }
 }
 
+/// Finds all valid QRs from the list of screenshots passed in.
+///
+/// From all screenshots, it will transform into a grayscale luma image
+/// then it will run the qr reader (from `rqrr`) to prepare and read all
+/// QR grids it can locate.
+///
+/// NOTE: this doesn't find a lot of variants of QRs, maybe worth looking into
+/// other libraries that can identify and decode more -- even barcodes?
 fn find_all_qrs(imgs: Vec<RgbaImage>) -> Vec<String> {
     imgs.into_iter()
         .flat_map(|i| {
@@ -172,8 +210,9 @@ fn find_all_qrs(imgs: Vec<RgbaImage>) -> Vec<String> {
         .collect::<Vec<String>>()
 }
 
+/// Main QRusty
 fn main() {
-    // set up the matches
+    // Set up the command line arguments
     let arg_matches = Command::new("qrusty")
         .version("0.1")
         .about("Read QR codes from an area of the screen, or the full screen")
@@ -186,17 +225,14 @@ fn main() {
         )
         .get_matches();
 
+    // Call the appropriate function to capture the area or the full screen.
     if let Ok(Some(true)) = arg_matches.try_get_one::<bool>("select") {
         if let Ok(reg) = get_region_from_user() {
             if let Ok(ss) = get_screen_shot(&reg) {
-                // find_qr(&ss)
                 println!("{:?}", find_all_qrs(vec![ss]));
             };
         }
-    } else {
-        if let Ok(images) = screenshot_all_monitors() {
-            println!("{:?}", find_all_qrs(images));
-        }
-        println!("Capturing entire screen");
+    } else if let Ok(images) = screenshot_all_monitors() {
+        println!("{:?}", find_all_qrs(images));
     }
 }
